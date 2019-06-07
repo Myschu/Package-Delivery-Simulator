4a68d070424433ef746a4595f3aa89faf5c34cd1
Myschu <bykko7@gmail.com>
2019-06-06 22:25:31 -0400

    Simple interactable map
    Updated logic scripts

---
+++ Gaym/Assets/Scripts/Map.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+    public GameObject button1, button2, path;
+
+    void Update()
+    {
+        bool onoff1 = button1.GetComponent<Toggle>().isOn;
+        bool onoff2 = button2.GetComponent<Toggle>().isOn;
+        if (onoff1 == true && onoff2 == true)
+        {
+            path.GetComponent<Image>().color = Color.red;
+        }
+        else path.GetComponent<Image>().color = Color.gray;
+    }
+}
