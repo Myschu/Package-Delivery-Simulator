48aaea6eb4a31b3e907ff6bc7cd4ef0cffd81c04
Myschu <bykko7@gmail.com>
2019-06-13 21:30:14 -0400

    Updated Interactable Map
    Changed which nodes are interactable at what times.

---
+++ Gaym/Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    public string TagName = "Map";
+    public bool NodeOn = false;
+    public bool Interactable = false;
+    public GameObject Button;
+    public GameObject Map;
+    void Start()
+    {
+        Map = GameObject.FindGameObjectWithTag(TagName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Button.GetComponent<Toggle>().isOn = NodeOn;
+        Button.GetComponent<Toggle>().interactable = Interactable;
+        Button.GetComponent<Toggle>().onValueChanged.AddListener(delegate { TaskOnClick(); });
+    }
+
+    void TaskOnClick()
+    {
+        NodeOn = !NodeOn;
+        Map map = Map.GetComponent<Map>();
+        if (NodeOn)
+        {
+            map.LastSelected.Add(Button);
+        }
+        else
+        {
+            map.LastSelected.Remove(Button);
+        }
+    }
+}
