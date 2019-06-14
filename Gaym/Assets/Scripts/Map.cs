5ec1ef4d34d044b3362b632917ec3015b97b5f8b
GeneHFang <gene.li@nyu.edu>
2019-06-13 23:27:06 -0400

    6.13 Meeting (contains Dan Branch)

--- Gaym/Assets/Scripts/Map.cs
+++ Gaym/Assets/Scripts/Map.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
 public class Map : MonoBehaviour
 {
     public GameObject button1, button2, path;
@@ -20,4 +21,106 @@ public class Map : MonoBehaviour
         }
         else path.GetComponent<Image>().color = Color.gray;
     }
+}
+*/
+public class Map : MonoBehaviour
+{
+    public Material line_material;
+    public string TagName = "Node";
+    public string TagName2 = "Path";
+    public GameObject[] Buttons;
+    //public GameObject[] Paths;
+    public GameObject Origin;
+    public List<GameObject> LastSelected = new List<GameObject>();
+    void Start()
+    {
+        Buttons = GameObject.FindGameObjectsWithTag(TagName);
+        Node button = Origin.GetComponent<Node>();
+        button.NodeOn = true;
+        LastSelected.Add(Origin);
+
+        //Paths = GameObject.FindGameObjectsWithTag(TagName2);
+        /*
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material.color = Color.red;
+        */
+    }
+    private void Update()
+    {   
+        foreach (GameObject node in Buttons)
+        {
+            Node button = node.GetComponent<Node>();
+            button.Interactable = false;
+        }
+        int number = LastSelected.Count;
+        int size_of_buttons = Buttons.Length;
+        GameObject Button = LastSelected[number - 1];
+        int index = System.Array.IndexOf(Buttons, Button);
+        Node curr_button = Buttons[index].GetComponent<Node>();
+        curr_button.Interactable = true;
+        if (index + 3 <= size_of_buttons - 1)
+        {
+            Node button = Buttons[index + 3].GetComponent<Node>();
+            if (button.NodeOn == false)
+            {
+                button.Interactable = true;
+            }
+        }
+        if (index - 3 >= 0)
+        {
+            Node button = Buttons[index - 3].GetComponent<Node>();
+            if (button.NodeOn == false)
+            {
+                button.Interactable = true;
+            }
+        }
+        if (index%3 != 2 && index + 1 <= size_of_buttons - 1)
+        {
+            Node button = Buttons[index + 1].GetComponent<Node>();
+            if (button.NodeOn == false)
+            {
+                button.Interactable = true;
+            }
+        }
+        if (index%3 != 0 && index - 1 >= 0)
+        {
+            Node button = Buttons[index - 1].GetComponent<Node>();
+            if (button.NodeOn == false)
+            {
+                button.Interactable = true;
+            }
+        }
+        /*
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        for (int i = 0; i < number - 1; i++)
+        {
+            for (int j = 1; j < number - 2; j++)
+            {
+                lineRenderer.SetPosition(0, new Vector3(LastSelected[i].transform.position.x, LastSelected[i].transform.position.y, LastSelected[i].transform.position.z));
+                lineRenderer.SetPosition(1, new Vector3(LastSelected[j].transform.position.x, LastSelected[j].transform.position.y, LastSelected[j].transform.position.z));
+            }
+        }
+        */
+    }
+    /*
+    void OnPostRender()
+    {   
+        GL.PushMatrix();
+        line_material.SetPass(0);
+        GL.LoadOrtho();
+        int number = LastSelected.Count;
+        for (int i = 0; i < number - 1; i++)
+        {
+            for (int j = 1; j < number - 2; j++)
+            {
+                GL.Begin(GL.LINES);
+                GL.Color(Color.red);
+                GL.Vertex(new Vector3(LastSelected[i].transform.position.x, LastSelected[i].transform.position.y, LastSelected[i].transform.position.z));
+                GL.Vertex(new Vector3(LastSelected[j].transform.position.x, LastSelected[j].transform.position.y, LastSelected[j].transform.position.z));
+            }
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+    */
 }
---
+++ Gaym/Assets/Scripts/Node.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    public string TagName = "Map";
+    public bool NodeOn = false;
+    public bool Interactable = false;
+    public GameObject Button;
+    public GameObject Map;
+    private int tog; 
+    void Start()
+    {
+        tog = 0;
+        Map = GameObject.FindGameObjectWithTag(TagName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Button.GetComponent<Toggle>().isOn = NodeOn;
+        Button.GetComponent<Toggle>().interactable = Interactable;
+        /** if( Button.GetComponent<Toggle>().onValueChanged.Equals(true) && x == true) {
+             x = false;
+             TaskOnClick();
+         }*/
+
+        Invoke("activate", 1.0f);
+        
+        
+    }
+    void activate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (tog == 0)
+            {
+                tog++;
+                Button.GetComponent<Toggle>().onValueChanged.AddListener(delegate
+                {
+                    TaskOnClick(NodeOn);
+
+                });
+            }
+        }
+    }
+
+    void TaskOnClick(bool val)
+    {
+        Debug.Log("clicked");
+        val = !val;
+        Map map = Map.GetComponent<Map>();
+        if (val)
+        {
+            map.LastSelected.Add(Button);
+            
+        }
+        else
+        {
+            map.LastSelected.Remove(Button);
+        }
+
+        NodeOn = val;
+        tog = 0;
+    }
+}
