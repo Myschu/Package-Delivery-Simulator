8361534f469c5c689cbc8fc4053face759f1672f
Myschu <bykko7@gmail.com>
2019-06-16 23:06:48 -0400

    Scene Change

---
+++ Gaym/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void ChangeToScene (string SceneToChangeTo)
+    {
+        SceneManager.LoadScene(SceneToChangeTo, LoadSceneMode.Single);
+    }
+}
