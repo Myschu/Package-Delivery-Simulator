b5f0ee41ac9e10081f362e00d4392076074f3c0f
Peenfest <gene.li@nyu.edu>
2019-06-16 21:50:24 -0400

    Line Render 6/16

--- Gaym/Assets/Scripts/GenerateLine.cs
+++ Gaym/Assets/Scripts/GenerateLine.cs
@@ -1,24 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateLine : MonoBehaviour
 {
 
-    public Transform[] points;
+    private LineRenderer line_renderer;
+    private float counter;
+    private float distance;
+
+
+    public Transform origin;
+    public Transform destination;
+    public GameObject button;
+
+  
+
+    void Start()
+    {
+        //Uncaching component
+        //Essentially avoids overhead
+        line_renderer = GetComponent<LineRenderer>();
+        //        this.transform.SetPositionAndRotation(origin.position, new Quaternion());
+        line_renderer.positionCount = 2;
+        line_renderer.SetPosition(0, new Vector2(origin.position.x,origin.position.y));
+        
+       
+
+
+
+    }
 
     // Start is called before the first frame update
     void Update()
     {
-        LineRenderer line = new LineRenderer();
+       
+        if (button.GetComponent<Toggle>().isOn)
+        //if (Input.GetKeyDown("space"))
+         {
+           // Debug.Log("Should be drawing this damn line");
+            line_renderer.SetPosition(1, new Vector2(destination.position.x, destination.position.y));
+        }
+        else
+        {
+            line_renderer.SetPosition(1, new Vector2(origin.position.x, origin.position.y));
+        }
+        
+        /*
+        if(Input.GetKeyDown("space"))
+        {
+            //if (button.GetComponent<Toggle>().isOn) { Debug.Log("is on"); }
+            //if (!(button.GetComponent<Toggle>().isOn)) { Debug.Log("is off"); }
+            Debug.Log(origin.position.x);
+            Debug.Log(origin.position.y);
+            Debug.Log(origin.position.z);
+            Debug.Log(destination.position.x);
+            Debug.Log(destination.position.y);
+            Debug.Log(destination.position.z);
 
-    
-
-        line.SetPosition(0, points[0].position);
-        line.SetPosition(1, points[1].position);
-
-
-
+        }
+        */
 
     }
 
