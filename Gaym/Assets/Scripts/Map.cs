a7b3985b9041836f68fc05af47f8dd2d04ab3f47
Myschu <bykko7@gmail.com>
2019-06-20 17:52:54 -0400

    Script Cleanup

--- Gaym/Assets/Scripts/Map.cs
+++ Gaym/Assets/Scripts/Map.cs
@@ -3,26 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/*
-public class Map : MonoBehaviour
-{
-    public GameObject button1, button2, path;
-    //public Transform target;
 
-    void Update()
-    {
-        bool onoff1 = button1.GetComponent<Toggle>().isOn;
-        bool onoff2 = button2.GetComponent<Toggle>().isOn;
-        if (onoff1 == true && onoff2 == true)
-        {
-            //Gizmos.color = Color.red;
-            //Gizmos.DrawLine(transform.position, target.position);
-            path.GetComponent<Image>().color = Color.red;
-        }
-        else path.GetComponent<Image>().color = Color.gray;
-    }
-}
-*/
 public class Map : MonoBehaviour
 {
     public Material line_material;
