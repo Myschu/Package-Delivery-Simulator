0f5584ba0c80b2b51c01cb6c2c4a1882c20ad897
Peenfest <gene.li@nyu.edu>
2019-07-30 20:58:59 -0400

    Changed name, Changed logic for delivery

---
+++ PackageSimGame/Assets/Scripts/House_Trigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House_Trigger : MonoBehaviour
+{
+
+    private int extraTime;
+    private Clock clock;
+    private bool clockFlag;
+    public House_Shared_Values houseHandler;
+    //private Stack<Package>
+
+   void Awake()
+    {
+        extraTime = Random.Range(0, 3);
+        clock = Object.FindObjectOfType<Clock>();
+        clockFlag = true;
+
+        //houseHandler = GameObject.FindGameObjectWithTag("HouseHandler").Get;
+
+    }
+
+  void OnTriggerEnter2D(Collider2D Truck)
+    {
+        string thisObj = name;
+        if (clockFlag)
+        {
+            clock.Ticker(30 + extraTime);
+            clockFlag = false;
+        }
+        Debug.Log("Truck entered delivery space of : "+thisObj);
+        Debug.Log("There are "+houseHandler.subtractPackage()+" left");
+
+
+
+    }
+
+    void OnTriggerExit2D(Collider2D Truck)
+    {
+        clockFlag = true;
+    }
+}
