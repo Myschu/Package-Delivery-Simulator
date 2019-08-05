12b9cbd0bcd0552b02ac70c3b1ca8f7f668b0efc
Peenfest <gene.li@nyu.edu>
2019-08-05 11:48:42 -0400

    slight changes

---
+++ PackageSimGame/Assets/Scripts/Return.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Return : MonoBehaviour
+{
+    public void OnClick(string SceneName)
+    {
+        SceneManager.UnloadSceneAsync(SceneName);
+    }
+
+    public void loadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+
+    }
+}
